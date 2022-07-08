29909d77a16ee5ec8aa07239f9e1c9981b0a1426
Daniel Kwon <skansmfqh7312@gmail.com>
2022-07-09 07:01:11 +0900

    Initial Commit

---
+++ newCoinedWordProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newCoinedWordProject
+//
+//  Created by 권민서 on 2022/07/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
