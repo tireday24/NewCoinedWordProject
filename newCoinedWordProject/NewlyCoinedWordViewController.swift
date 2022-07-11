04c390c43e0766128adc55e8ee246f7966e592cf
Daniel Kwon <skansmfqh7312@gmail.com>
2022-07-11 16:19:10 +0900

    func 추가

--- newCoinedWordProject/NewlyCoinedWordViewController.swift
+++ newCoinedWordProject/NewlyCoinedWordViewController.swift
@@ -21,12 +21,12 @@ class NewlyCoinedWordViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         designTextField()
-        buttonDesign(firstButton, buttonTitle: "11111111111", buttonBackgroundColor: .white)
-        buttonDesign(secondButton, buttonTitle: "11111111", buttonBackgroundColor: .white)
+        buttonDesign(firstButton, buttonTitle: "윰차", buttonBackgroundColor: .white)
+        buttonDesign(secondButton, buttonTitle: "실매", buttonBackgroundColor: .white)
         buttonDesign(thirdButton, buttonTitle: "만반잘부", buttonBackgroundColor: .white)
         buttonDesign(fourthButton, buttonTitle: "꾸안꾸", buttonBackgroundColor: .white)
         //buttonLoopDesign()
-        
+        randomNewCoinedWord()
         
 
         
@@ -51,6 +51,16 @@ class NewlyCoinedWordViewController: UIViewController {
 //        }
 //    }
     
+    var newCoinedWord : [String : String] = [
+        "삼귀자" : "사귀기전 썸을 시작하는 단계",
+        "버카충" : "버스 카드 충전",
+        "만반잘부" : "만나서 잘 부탁드립니다",
+        "꾸안꾸" : "꾸민듯 안 꾸민듯",
+        "알잘딱깔센" : "알아서 잘하고 딱 깔금하고 센스있다"
+        
+    ]
+        
+    
     func buttonDesign(_ buttonName : UIButton, buttonTitle: String ,buttonBackgroundColor bgcolor: UIColor) {
         buttonName.setTitle(buttonTitle, for: .normal)
         buttonName.setTitleColor(.black, for: .normal)
@@ -58,10 +68,23 @@ class NewlyCoinedWordViewController: UIViewController {
         buttonName.layer.borderWidth = 1
         buttonName.backgroundColor = bgcolor
         buttonName.layer.cornerRadius = 8
-        
-
-        
     }
+    
+    func randomNewCoinedWord() {
+        var randomKeys : [String] = []
+
+        while randomKeys.count < 4 {
+            let randomkey = newCoinedWord.keys.randomElement()!
+            
+            if !randomKeys.contains(randomkey) {
+                randomKeys.append(randomkey)
+            }
+            print(randomKeys.sorted())
+            
+        }
+            
+        }
+    
     
     @IBAction func readingGlassesButtonClicked(_ sender: UIButton) {
         
