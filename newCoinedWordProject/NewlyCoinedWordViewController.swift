2ddf2a9375d9d1209fb80b7568878c00abb7408b
Daniel Kwon <skansmfqh7312@gmail.com>
2022-07-12 14:32:57 +0900

    랜덤 버튼 추가

--- newCoinedWordProject/NewlyCoinedWordViewController.swift
+++ newCoinedWordProject/NewlyCoinedWordViewController.swift
@@ -11,24 +11,28 @@ class NewlyCoinedWordViewController: UIViewController {
 
     
     @IBOutlet weak var wordTextField: UITextField!
-    @IBOutlet weak var firstButton: UIButton!
-    @IBOutlet weak var secondButton: UIButton!
-    @IBOutlet weak var thirdButton: UIButton!
-    @IBOutlet weak var fourthButton: UIButton!
+    @IBOutlet var bottomMenuButton : [UIButton]!
     @IBOutlet weak var readingGlassesButton: UIButton!
     @IBOutlet weak var wordResultLabel: UILabel!
+    
+    var newCoinedWord : [String : String] = [
+        "삼귀자" : "사귀기전 썸을 시작하는 단계",
+        "버카충" : "버스 카드 충전",
+        "만반잘부" : "만나서 잘 부탁드립니다",
+        "꾸안꾸" : "꾸민듯 안 꾸민듯",
+        "알잘딱깔센" : "알아서 잘하고 딱 깔금하고 센스있다",
+        "700" : "귀여워",
+        "우유남" : "우월한 유전자를 가진 남자",
+        "점메추" : "점심 메뉴 추천",
+        "빠태" : "빠른 태세 전환",
+        "나일리지": "나이 + 마일리지, 꼰대를 의미"
+        
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         designTextField()
-        buttonDesign(firstButton, buttonTitle: "윰차", buttonBackgroundColor: .white)
-        buttonDesign(secondButton, buttonTitle: "실매", buttonBackgroundColor: .white)
-        buttonDesign(thirdButton, buttonTitle: "만반잘부", buttonBackgroundColor: .white)
-        buttonDesign(fourthButton, buttonTitle: "꾸안꾸", buttonBackgroundColor: .white)
-        //buttonLoopDesign()
-       
-        
-
+        randomNewCoinedWord()
         
     }
     
@@ -40,50 +44,62 @@ class NewlyCoinedWordViewController: UIViewController {
         wordTextField.textAlignment = .left
         wordTextField.layer.borderColor = UIColor.black.cgColor
         
+    
+    
+    for btn in bottomMenuButton {
+        btn.backgroundColor = .white
+        btn.layer.borderWidth = 1
+        btn.layer.borderColor = UIColor.black.cgColor
+        btn.setTitleColor(.black, for: .normal)
+        
     }
-
-    
-    
-    let newCoinedWord : [String : String] = [
-        "삼귀자" : "사귀기전 썸을 시작하는 단계",
-        "버카충" : "버스 카드 충전",
-        "만반잘부" : "만나서 잘 부탁드립니다",
-        "꾸안꾸" : "꾸민듯 안 꾸민듯",
-        "알잘딱깔센" : "알아서 잘하고 딱 깔금하고 센스있다"
         
-    ]
+        wordResultLabel.textColor = .black
+        wordResultLabel.text = "안녕하세요!"
+        wordResultLabel.font = UIFont.systemFont(ofSize: 20)
         
-    
-    func buttonDesign(_ buttonName : UIButton, buttonTitle: String ,buttonBackgroundColor bgcolor: UIColor) {
-        buttonName.layer.borderColor = UIColor.black.cgColor
-        buttonName.layer.borderWidth = 1
-        buttonName.backgroundColor = bgcolor
-        buttonName.layer.cornerRadius = 8
     }
     
-    
-    
-    
-    
+    func randomNewCoinedWord() {
+        var randomKeys : [String] = []
+        
+        while randomKeys.count < 4 {
+            let randomkey = newCoinedWord.keys.randomElement()!
+            
+            if !randomKeys.contains(randomkey) {
+                randomKeys.append(randomkey)
+            }
+            print(randomKeys.sorted())
+        }
+        
+        for i in 0...3 {
+            bottomMenuButton[i].setTitle("\(randomKeys[i])", for: .normal)
+            bottomMenuButton[i].setTitleColor(.black, for: .normal)
+            bottomMenuButton[i].backgroundColor = .white
+            bottomMenuButton[i].layer.cornerRadius = 10
+            bottomMenuButton[i].clipsToBounds = true
+            bottomMenuButton[i].layer.borderWidth = 1
+            bottomMenuButton[i].layer.borderColor = UIColor.black.cgColor
+        }
+        
+        
+    }
     
     @IBAction func readingGlassesButtonClicked(_ sender: UIButton) {
         
         if !(wordTextField.text?.isEmpty)! {
             wordResultLabel.text = newCoinedWord[wordTextField.text!]
         }
-        
-
-        
-        
+        randomNewCoinedWord()
     }
     
     @IBAction func tapGestureClicked(_ sender: UITapGestureRecognizer) {
-        view.backgroundColor = .white
         view.endEditing(true)
     }
     
     
     @IBAction func didEndOnExit(_ sender: UITextField) {
+        randomNewCoinedWord()
         view.endEditing(true)
     }
     
